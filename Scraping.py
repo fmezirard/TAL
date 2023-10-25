1c9bba969e34994fea76d389f90a191e570c719a
fmezirard <flor.mezirard@gmail.com>
2023-10-25 17:33:14 +0200

    ajout du fichier

---
+++ Scraping.py
@@ -0,0 +1,18 @@
+# Projet TAL
+
+#########################
+### Floriane Mézirard ###
+### Lucie Raimbault   ###
+#########################
+
+
+# Web Scraping
+
+#########################
+# 1- Récupération des liens sur la page Web Seek -- moteur de recherche d'emploi en Australie
+
+#########################
+# 2 - Pour les liens récupérés -- sauvagarder les éléments souhaités dans un dataframe + si review sauvarger les liens
+
+#########################
+# 3 - Si lien de review -- récupérer les commentaires et notes sur l'entreprise
